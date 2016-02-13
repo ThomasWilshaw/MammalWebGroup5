a7da7e8ae9f1db8ed6b92616bc1cab9a18b23a67
Will Taylor <w1ll100@hotmail.co.uk>
2016-02-13 19:26:07 +0000

    Formatting for searching
    Added table structuring for output of SQL, should be easy to extend with
    an array of the input field names from dropdowns

--- main/phpDBTest.php
+++ main/phpDBTest.php
@@ -3,6 +3,7 @@
 	<title>Php x SQL</title>
 </head>
 <body>
+	<a href="#">Back</a>  <!--  MAKE THIS POINT BACK TO THE SEARCH pAGE  -->
 	<?php 
 		$servername="localhost";
 		$username="root";
@@ -11,10 +12,8 @@
 
 		$connection=new mysqli($servername,$username,$password,$dbname);
 
-		$speciesmap=loadSpeciesMap($connection);
-		//Get species list
-		
-		echo print_r($speciesmap);
+		//Get species list - $speciesMap holds an associative array of number(int)->species(string) as found in the options table
+		$speciesmap=loadSpeciesMap($connection);		
 
 		//Echos photo id for all photos whose unevenness> some threshold ie all the photos that need to be assessed by scientist
 		$uneventhreshold=0.75;
@@ -23,35 +22,48 @@
 
 		echo "<h2>High unevenness results:</h2><br>";
 
+		//This is an easy way to structure the output table, have some string combination thing for all the passed ion variables (from dropdowns) that define the columns as well as for the SQL queries
+		echo "<table>";
+		echo "<tr><td>Photo ID</td></tr>";
 		if($uneven->num_rows>0){
 			while($row=$uneven->fetch_assoc()){
-				echo "Photo ID: ".$row["photo_id"]."<br>";
+				echo "<tr>";
+				echo "<td>".$row["photo_id"]."</td>";
+				echo "</tr>";
 			}
 		}
 		else{
-			echo "No uneven results";
+			echo "<tr><td>No uneven results</td></tr>";
 		}
+		echo "</table>";
 
 		//Selects the photo ids for useful photos
-
 		$sql="SELECT * FROM aggregate WHERE evenness>=0 AND evenness<".$uneventhreshold.";";
 		$even=$connection->query($sql);
 
 		echo "<br><br><h2>Good results:</h2><br>";
 
+		echo "<table>";
+		echo "<tr><td>Photo ID</td><td>Species</td></tr>";
+
 		if($even->num_rows>0){
 			while($row=$even->fetch_assoc()){
-				echo "Photo ID: ".$row["photo_id"]."<br>";
+				echo"<tr>";
+				echo "<td>".$row["photo_id"]."</td>";
+				echo "<td>".$speciesmap[$row["species"]]."</td>";
+				echo"</tr>";
 			}
 		}
 		else{
-			echo "No useful results";
+			echo "<tr><td>No useful results</td></tr>";
 		}
+
+		echo "</table>";
 
 		$connection->close();
 
 		function loadSpeciesMap($connection){
-			$sql="SELECT option_id,option_name FROM options WHERE struc='bird' OR struc='mammal' OR struc='noanimal'";
+			$sql="SELECT option_id,option_name FROM options WHERE struc='bird' OR struc='mammal' OR struc='noanimal' OR struc='notinlist'";  //Really not happy with this line
 			$speciesquery=$connection->query($sql);
 
 			$speciesmap=array();
@@ -61,5 +73,6 @@
 			return $speciesmap;
 		}
 		?>
+	</table>
 </body>
 </html>
