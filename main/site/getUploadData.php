38cc7045aedd1f349a837d71d629dd5db972e086
Will Taylor <w1ll100@hotmail.co.uk>
2016-04-01 18:39:38 +0100

    PHP to get user's uploads

---
+++ main/site/getUploadData.php
@@ -0,0 +1,24 @@
+<?php
+
+	//$person_id = $_POST["person_id"];
+	$person_id=182;
+	include('config.php');
+	
+	//establish connection
+	$connection=new mysqli(DBHOST,DBUSER,DBPASS,DBNAME);
+
+	if ($connection->connect_errno) {
+	    echo "Failed to connect to MySQL: (" . $connection->connect_errno . ") " . $connection->connect_error;
+	}
+
+	$uploadArray=array();
+
+	$sql="SELECT * FROM upload WHERE person_id=".$person_id.";";
+	$uploadQuery=$connection->query($sql);
+	if($uploadQuery->num_rows>0){
+		while($row=$uploadQuery->fetch_assoc()){
+			$uploadArray[$row["upload_id"]]=$row["timestamp"];
+		}
+	}
+	echo $uploadArray;
+?>
